11f7fcdbae71ac7cf1d879b8c76b66bfac0261e8
Veeresh Tiranga <veereshtiranga123@gmail.com>
2025-05-20 14:46:25 +0530

    commit from india

---
+++ src/test/java/firstPackage/GitDemo.java
@@ -0,0 +1,12 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class GitDemo {
+	
+	@Test
+	public void gitDemoTest() {
+		System.out.println("I have made changes- from Indian guy");
+	}
+
+}
